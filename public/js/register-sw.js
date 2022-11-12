7a3fdaac6cadc6abe6c058a660d482bd42564914
sergio <sergiomancilla13@gmail.com>
2022-11-11 23:39:54 -0300

    tes2

--- public/js/register-sw.js
+++ public/js/register-sw.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('sw.js').then((message) => {
+    navigator.serviceWorker.register('service-worker.js').then((message) => {
         console.log("Service Worker esta listo");
     });
 
